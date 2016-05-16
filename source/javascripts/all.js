32c5451bb8bad52b03dcc8666303dc08ec5d1bd4
jakeprins <jakeprins@outlook.com>
2016-05-16 15:26:58 +0200

    Added resume

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,6 +2,15 @@
 //= require bootstrap-sprockets
 //= require tree .
 
+if(typeof jQuery!=='undefined'){
+    console.log('jQuery Loaded');
+}
+else{
+    console.log('not loaded yet');
+}
+
+// Scroll function
+// Some code thanks to Alex Devero
 $(function() {
   $('.js-nav a, .js-connect').click(function(e) {
     e.preventDefault();
@@ -10,3 +19,38 @@ $(function() {
     }, 750);
   });
 });
+
+// CV
+// Some code thanks to @chrisgannon
+var select = function(s) {
+  return document.querySelector(s);
+}
+
+function randomBetween(min,max)
+{
+    var number = Math.floor(Math.random()*(max-min+1)+min);
+
+    if ( number !== 0 ){
+      return number;
+    }else {
+      return 0.5;
+    }
+}
+
+var tl = new TimelineMax();
+
+for(var i = 0; i < 20; i++){
+
+  var t = TweenMax.to(select('.bubble' + i), randomBetween(1, 1.5), {
+    x: randomBetween(12, 15) * (randomBetween(-1, 1)),
+    y: randomBetween(12, 15) * (randomBetween(-1, 1)),
+    repeat:-1,
+    repeatDelay:randomBetween(0.2, 0.5),
+    yoyo:true,
+    ease:Elastic.easeOut.config(1, 0.5)
+  })
+
+  tl.add(t, (i+1)/0.6)
+}
+
+tl.seek(50);
