7aa59ebee99b4e13db78a5ea4bbfac55449ae875
jakeprins <jakeprins@outlook.com>
2016-05-19 10:40:15 +0200

    Use waypoints to make icons appear on scroll down

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -20,37 +20,15 @@ $(function() {
   });
 });
 
-// CV
-// Some code thanks to @chrisgannon
-var select = function(s) {
-  return document.querySelector(s);
-}
-
-function randomBetween(min,max)
-{
-    var number = Math.floor(Math.random()*(max-min+1)+min);
-
-    if ( number !== 0 ){
-      return number;
-    }else {
-      return 0.5;
+// Icons appear on scroll
+$(function(){
+  var icons = document.querySelector('.icons')
+  new Waypoint({
+    element: document.querySelector("#services"),
+    handler: function(direction) {
+      if ( direction === "down") {
+        icons.classList.add("is-active");
+      } 
     }
-}
-
-var tl = new TimelineMax();
-
-for(var i = 0; i < 20; i++){
-
-  var t = TweenMax.to(select('.bubble' + i), randomBetween(1, 1.5), {
-    x: randomBetween(12, 15) * (randomBetween(-1, 1)),
-    y: randomBetween(12, 15) * (randomBetween(-1, 1)),
-    repeat:-1,
-    repeatDelay:randomBetween(0.2, 0.5),
-    yoyo:true,
-    ease:Elastic.easeOut.config(1, 0.5)
-  })
-
-  tl.add(t, (i+1)/0.6)
-}
-
-tl.seek(50);
+  });
+});
